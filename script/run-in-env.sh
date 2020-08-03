Leonardcd88/core

commit ca3232cd8218d94432b96d9356a59d21ea26abeb

Fix pre-commit hook issue on Windows (#31648)
* Make shebang compatable with Windows

* Prehook test

* Undo prehook test

script/run-in-env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/usr/bin/env sh -eu
 
 # Activate pyenv and virtualenv if present, then run the specified command
 

commit eb4fe9deb2d3aa5f3da595dabd882ce040502ed8

Fix run-in-env.sh sh options (#38520)
Shebang takes only one arg, regression in
ca3232cd8218d94432b96d9356a59d21ea26abeb

script/run-in-env.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh -eu
+#!/usr/bin/env sh
+set -eu
 
 # Activate pyenv and virtualenv if present, then run the specified command
 