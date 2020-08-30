Leonardcd88/core

commit b88a3053d5f6aeb208d85d6c5d5e99092111856f

Huawei LTE device registry improvements (#38925)
* Fix getting software version in unauthenticated mode

* Add serial number device identifier if available

homeassistant/components/huawei_lte/__init__.py
@@ -163,6 +163,14 @@ class Router:
         return DEFAULT_DEVICE_NAME
 
     @property
+    def device_identifiers(self) -> Set[Tuple[str, str]]:
+        """Get router identifiers for device registry."""
+        try:
+            return {(DOMAIN, self.data[KEY_DEVICE_INFORMATION]["SerialNumber"])}
+        except (KeyError, TypeError):
+            return set()
+
+    @property
     def device_connections(self) -> Set[Tuple[str, str]]:
         """Get router connections for device registry."""
         return {(dr.CONNECTION_NETWORK_MAC, self.mac)} if self.mac else set()
@@ -390,6 +398,7 @@ async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigEntry) 
     device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
         connections=router.device_connections,
+        identifiers=router.device_identifiers,
         name=router.device_name,
         manufacturer="Huawei",
         **device_data,
@@ -594,7 +603,10 @@ class HuaweiLteBaseEntity(Entity):
     @property
     def device_info(self) -> Dict[str, Any]:
         """Get info for matching with parent router."""
-        return {"connections": self.router.device_connections}
+        return {
+            "identifiers": self.router.device_identifiers,
+            "connections": self.router.device_connections,
+        }
 
     async def async_update(self) -> None:
         """Update state."""

homeassistant/components/huawei_lte/const.py
@@ -53,4 +53,10 @@ SENSOR_KEYS = {
 
 SWITCH_KEYS = {KEY_DIALUP_MOBILE_DATASWITCH}
 
-ALL_KEYS = BINARY_SENSOR_KEYS | DEVICE_TRACKER_KEYS | SENSOR_KEYS | SWITCH_KEYS
+ALL_KEYS = (
+    BINARY_SENSOR_KEYS
+    | DEVICE_TRACKER_KEYS
+    | SENSOR_KEYS
+    | SWITCH_KEYS
+    | {KEY_DEVICE_BASIC_INFORMATION}
+)

commit aa96c20e07ac0a5e2eb7373efcc51ae79ce59d74

Fix huawei_lte duplicate device registry identifiers (#39409)
Regression in b88a3053d5f6aeb208d85d6c5d5e99092111856f

Refs https://github.com/home-assistant/core/pull/38925

homeassistant/components/huawei_lte/__init__.py
@@ -385,9 +385,6 @@ async def async_setup_entry(hass: HomeAssistantType, config_entry: ConfigEntry) 
     sw_version = None
     if router.data.get(KEY_DEVICE_INFORMATION):
         device_info = router.data[KEY_DEVICE_INFORMATION]
-        serial_number = device_info.get("SerialNumber")
-        if serial_number:
-            device_data["identifiers"] = {(DOMAIN, serial_number)}
         sw_version = device_info.get("SoftwareVersion")
         if device_info.get("DeviceName"):
             device_data["model"] = device_info["DeviceName"]